9cefaddbb1a5b3efe9eb2cb557929c811de42a11
pearseona <baesuna2930@gmail.com>
2026-07-11 04:41:43 +0900

    feat: 대결 결과 점수 repository 구현

---
+++ src/main/java/com/team6/server/ranking/repository/EpisodeRankingRepository.java
@@ -0,0 +1,4 @@
+package com.team6.server.ranking.repository;
+
+public interface EpisodeRankingRepository {
+}
---
+++ src/main/java/com/team6/server/ranking/repository/TitleRepository.java
@@ -0,0 +1,4 @@
+package com.team6.server.ranking.repository;
+
+public class TitleRepository {
+}
